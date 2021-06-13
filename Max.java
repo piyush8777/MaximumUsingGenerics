5fd9d19bb57d4f77cbb6ee3fdc6498958f010178
Piyush Shaw <piyushshaw8777@gmail.com>
2021-06-13 21:07:17 +0530

    Added :Max Float Number in First Position

--- Max.java
+++ Max.java
@@ -1,4 +1,30 @@
 
 public class Max {
+	 public static Integer findMaximum(Integer a, Integer b, Integer c){
+	        Integer max;
+	        max = a;
+	        if (b.compareTo(max) == 1)
+	            max = b;
+	        if (c.compareTo(max) == 1)
+	            max = c;
+	        return max;
+	    }
+
+	    public static void printMaximum(Integer m){
+	        System.out.println("Maximum Number is: "+m);
+	    }
+	    public static Float findMaximumFloat(Float a, Float b, Float c) {
+	        Float max;
+	        max = a;
+	        if (b.compareTo(max) == 1)
+	            max = b;
+	        if (c.compareTo(max) == 1)
+	            max = c;
+	        printFloatMaximum(max);
+	        return max;
+	    }
+	    public static void printFloatMaximum(Float m) {
+	        System.out.println("Maximum float Number is: " + m);
+	    }
 
 }
