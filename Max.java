adf7e9a0c747ce0d44aa79c86cfa8f0d47a4a39e
Piyush Shaw <piyushshaw8777@gmail.com>
2021-06-13 21:22:08 +0530

    Added :Refactor Maximum Class Using Generics

--- Max.java
+++ Max.java
@@ -1,46 +1,30 @@
 
-public class Max {
-	 public static Integer findMaximum(Integer a, Integer b, Integer c){
-	        Integer max;
-	        max = a;
-	        if (b.compareTo(max) == 1)
-	            max = b;
-	        if (c.compareTo(max) == 1)
-	            max = c;
-	        return max;
-	    }
+public class Max<E extends Comparable<E>> {
 
-	    public static void printMaximum(Integer m){
-	        System.out.println("Maximum Number is: "+m);
-	    }
-	    public static Float findMaximumFloat(Float a, Float b, Float c) {
-	        Float max;
-	        max = a;
-	        if (b.compareTo(max) == 1)
-	            max = b;
-	        if (c.compareTo(max) == 1)
-	            max = c;
-	        printFloatMaximum(max);
-	        return max;
-	    }
-	    public static void printFloatMaximum(Float m) {
-	        System.out.println("Maximum float Number is: " + m);
-	    }
+    E a, b, c;
 
-public static String findMaximumString(String a, String b, String c) {
-    String max;
-    max = a;
-    if (b.compareTo(max) == 1)
-        max = b;
-    if (c.compareTo(max) == 1)
-        max = c;
-    printStringMaximum(max);
-    return max;
-}
+    public Max(E a, E b, E c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
 
+    public E findMaximum() {
+        return findMaximum(a, b, c);
+    }
 
-public static void printStringMaximum(String m)
-{
-    System.out.println("Maximum String is: " + m);
-}
+    public static <E extends Comparable<E>> E findMaximum(E a, E b, E c) {
+        E max;
+        max = a;
+        if (b.compareTo(max) > 0)
+            max = b;
+        if (c.compareTo(max) > 0)
+            max = c;
+        printMaximum(max);
+        return max;
+    }
+
+    public static <T> void printMaximum(T m) {
+        System.out.println("The Maximum is: " + m);
+    }
 }
